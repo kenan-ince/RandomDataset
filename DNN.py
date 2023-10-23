74baa7df52d258eeff7579b5fa0522a920a644c1
Kenan İNCE <kenanince@gmail.com>
2023-10-23 09:52:56 +0300

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -1,4 +1,3 @@
-
 import pandas
 from keras.models import Sequential
 from keras.layers import Dense
@@ -7,17 +6,15 @@ from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import StratifiedKFold
 import tensorflow as tf
+import os
 
-from tensorflow.keras.layers import Embedding, LSTM, Dense
-
-
-class lstm:
+class dnn:
     def fileWalker(file, dirName):
         dataframe = pandas.read_csv(dirName + "/1000.csv", low_memory=False, memory_map=True, header=None)
         dataset = dataframe.values
         # split into input (X) and output (Y) variables
 
-        data = {"Frequency": 25,
+        data = {"Frequency": 100,
                 "BlockFrequency": 100,
                 "Runs": 100,
                 "LongestRunOfOnes": 128,
@@ -45,22 +42,23 @@ class lstm:
         tf.experimental_relax_shapes=True;
 
         # estimator = KerasClassifier(build_fn=create_baseline, epochs=10, batch_size=50, verbose=0)
-        estimator = KerasClassifier(build_fn=lstm.create_baseline, input_dim=dataWidth, epochs=10, batch_size=50, verbose=0)
-        kfold = StratifiedKFold(n_splits=5, shuffle=True)
+        estimator = KerasClassifier(build_fn=dnn.create_baseline, input_dim=dataWidth, epochs=10, batch_size=50, verbose=0)
+        kfold = StratifiedKFold(n_splits=10, shuffle=True)
         # kfold = StratifiedKFold(n_splits=10, shuffle=True)
         results = cross_val_score(estimator, X, encoded_Y, cv=kfold)
 
-        f = open("./Results/" + file + ".txt", "w")
+        f = open("./Results2/" + file + ".txt", "w")
         f.write("%s %s Baseline: %.4f%% (%.4f%%)" % (file, file, results.mean() * 100, results.std() * 100))
         print("%s Baseline: %.4f%% (%.4f%%)" %  (file, results.mean() * 100, results.std() * 100))
+
 
     def create_baseline(input_dim=None):
         # create model
         model = Sequential()
-        model.add(Embedding(input_dim=input_dim, output_dim=1))
-        model.add(LSTM(units=1))
-        model.add(Dense(units=1, activation='sigmoid'))
-        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        model.add(Dense(input_dim, input_dim=input_dim, activation='relu'))
+        model.add(Dense(input_dim/2, input_dim=input_dim/2, activation='sigmoid'))
+        model.add(Dense(input_dim/4, input_dim=input_dim/4, activation='softmax'))
+        model.add(Dense(1, activation='softmax'))
+        # Compile model
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
         return model
-
-
